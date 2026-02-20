5148199d2d91d30bd61ad17d2c265139d1a82c42
Gunalvijay <prasath12348@gmail.com>
2026-02-20 17:40:40 +0530

    Effect component added

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,12 +3,13 @@ import Button from "./Examples/Button";
 import List from "./Examples/List";
 import Counter from "./Examples/Counter";
 import Demo from "./Examples/Render";
+import Effect from "./Examples/Effect";
 
 function App() {
 
   return (
-    <Demo />
-  );
+    <Effect />
+  )
 }
 
 
---
+++ my-react-app/src/Examples/Effect.jsx
@@ -0,0 +1,38 @@
+//useEffect(function, [dependencies])
+// 1. useEffect(() => {})           // Runs after every re-render
+// 2. useEffect(() => {}, [])       // Runs only on mount
+// 3. useEffect(() => {}, [value])  // Runs on mount + when value changes
+
+import React, {useState, useEffect} from "react";
+
+function MyComponent() {
+
+    const [width, setWidth] = useState(window.innerWidth);
+    const [height, setHeight] = useState(window.innerHeight);
+
+    useEffect(() => {
+        window.addEventListener("resize", handleResize);
+        console.log("EVENT LISTENER ADDED");
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+            console.log("EVENT LISTENER REMOVED");
+        }
+    }, []);
+
+    useEffect(() => {
+        document.title = `Size: ${width} x ${height}`;
+    }, [width, height]);
+
+    function handleResize(){
+        setWidth(window.innerWidth);
+        setHeight(window.innerHeight);
+    }
+
+    return (<>
+        <p>Window Width: {width}px</p>
+        <p>Window Height: {height}px</p>
+    </>);
+}
+
+export default MyComponent;
