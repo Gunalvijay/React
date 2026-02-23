69588be18d18e3178f8da5ee0b4befdf61dffb61
Gunalvijay <prasath12348@gmail.com>
2026-02-23 11:27:48 +0530

    useContext component added

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,14 +1,16 @@
+import React from "react";
 import Card from "./Examples/Card";
 import Button from "./Examples/Button";
 import List from "./Examples/List";
 import Counter from "./Examples/Counter";
 import Demo from "./Examples/Render";
 import Effect from "./Examples/Effect";
+import ComponentA from "./Examples/UseContext/ComponentA";
 
 function App() {
 
   return (
-    
+    <ComponentA />
   )
 }
 
---
+++ my-react-app/src/Examples/UseContext/ComponentC.jsx
@@ -0,0 +1,12 @@
+import ComponentD from './ComponentD.jsx';
+
+function ComponentC(){
+
+    return(
+        <div className={styles.box}>
+            <h1>ComponentC</h1>
+            <ComponentD />
+        </div>
+    );
+}
+export default ComponentC
